62c6e563d1e6663fd32b6576ff39a7842d69d93c
jancarlohendriks <jancarlohendriks@hotmail.com>
2022-05-05 16:55:27 +0200

    minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import * as dat from 'dat.gui'
 
 const scene = new THREE.Scene() // SCENE
+const gui = new dat.GUI() // GUI
 scene.add(new THREE.AxesHelper(5)) // AXES HELPER
 const stats = Stats() // STATS
 document.body.appendChild(stats.dom)
@@ -19,6 +21,7 @@ scene.add(ambientLight)
 // CAMERA
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1,1000)
 camera.position.z = 2
+gui.add(camera.position, 'z').min(-5).max(10)
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer()
@@ -31,17 +34,26 @@ controls.enableDamping = true
 // controls.enableZoom = false
 
 // LOADER
-let model
+let model, sceneCamera, clip, mixer
 const loader = new GLTFLoader()
 loader.load(
 	'models/model-4.glb',
 	function (gltf) {
 		model = gltf.scene
+		sceneCamera = gltf.cameras[0]
+		clip = THREE.AnimationClip.findByName( gltf.animations, 'Action' )  //get the animation name by console.log(gltf.animations)
+		mixer = new THREE.AnimationMixer(sceneCamera)
+		const action = mixer.clipAction(clip)
+		action.play()
 		scene.add(gltf.scene)
 	},
 	(xhr) => console.log((xhr.loaded / xhr.total) * 100 + '% loaded'),
 	(error) => console.log(error)
 )
+
+// CLOCK
+const clock = new THREE.Clock()
+let previousTime = 0
 
 // RENDER
 function render() { renderer.render(scene, camera) }
@@ -49,8 +61,24 @@ function render() { renderer.render(scene, camera) }
 // ANIMATE
 function animate() {
 	requestAnimationFrame(animate)
+	// controls.update()
 	render()
 	stats.update()
+	// camera.updateProjectionMatrix()
+	// console.log(camera.position.z)
+
+	const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+	// console.log(deltaTime)
+	// mixer.update(deltaTime / 1000)
+	// if(mixer !== null){
+	// }
+
+	//Don't forget to render with the blender camera
+	if(model !== null){
+		// renderer.render(scene, sceneCamera)
+		// sceneCamera.updateProjectionMatrix()
+	}
 }
 animate()
 
@@ -60,4 +88,12 @@ function onWindowResize() {
 	camera.updateProjectionMatrix()
 	renderer.setSize(window.innerWidth, window.innerHeight)
 	render()
+}
+
+let i = 0;
+window.addEventListener('wheel', onScroll, false)
+function onScroll(e) {
+	if (i > 0 && e.deltaY < 0) i--
+	if (i < 100 && e.deltaY > 0) i++
+	// console.log(i)
 }
